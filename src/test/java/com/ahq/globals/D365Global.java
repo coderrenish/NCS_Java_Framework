c456660852ed8efa1044942da731eb4ca176bb50
coderrenish <coderrenish@users.noreply.github.com>
2023-12-22 19:25:39 +1000

    Changes

--- src/test/java/com/ahq/globals/D365Global.java
+++ src/test/java/com/ahq/globals/D365Global.java
@@ -3,164 +3,252 @@ package com.ahq.globals;
 import com.qmetry.qaf.automation.step.QAFTestStep;
 import com.ahq.utils.loc;
 import com.qmetry.qaf.automation.ui.WebDriverTestBase;
-import org.openqa.selenium.Keys;
+import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebElement;
+import com.qmetry.qaf.automation.ui.webdriver.QAFWebDriver;
+import com.qmetry.qaf.automation.ui.webdriver.QAFWebElement;
 import org.openqa.selenium.interactions.Actions;
-
+import org.openqa.selenium.interactions.WheelInput;
 
 public class D365Global {
 
     /**
-     * @param value [Value to fill]
+     * @param text [text to fill]
      * @param field [Field name]
      * @param page [Page name]
      */
-    @QAFTestStep(description = "MSDynamics: Input:{0} field:{1} page:{2}")
-    public static void input(String value,String field,String page) throws Exception {
+    @QAFTestStep(description = "MSDynamics: Input Text:{0} Field:{1} Page:{2}")
+    public static void inputText(String text,String field,String page) throws Exception {
         BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"input",field));
         BrowserGlobal.iScrollToAnElement(loc.get(page,"input",field));
         BrowserGlobal.iClickOn(loc.get(page,"input",field));
-        BrowserGlobal.iWaitForMilliseconds("500");
-        BrowserGlobal.iInputInTo(value,loc.get(page,"input",field));
-    }
-
-    /**
-     * @param date [Date to fill]
-     * @param field [Field name]
-     * @param page [Page name]
-     */
-    @QAFTestStep(description = "MSDynamics: Input-date:{0} field:{1} page:{2}")
-    public static void inputDate(String date,String field,String page) throws Exception {
-//        BrowserGlobal.iDoubleClickAndFillInTo(date,loc.get(page,"input",field));
-        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"input",field));
-        BrowserGlobal.iClickOn(loc.get(page,"input",field));
-        BrowserGlobal.iWaitForSeconds("2");
-        BrowserGlobal.iClickOn(loc.get(page,"input",field));
-        BrowserGlobal.iWaitForSeconds("2");
-        BrowserGlobal.iInputInTo(date,loc.get(page,"input",field));
-    }
-
-    /**
-     * @param date [Date to fill]
-     * @param field [Field name]
-     * @param page [Page name]
-     */
-    @QAFTestStep(description = "MSDynamics: Input-date:{0} by-clearing field:{1} page:{2}")
-    public static void inputDateByClearing(String date,String field,String page) throws Exception {
-
-        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"input",field));
-        BrowserGlobal.iClickOn(loc.get(page,"input",field));
-        BrowserGlobal.iWaitForSeconds("2");
-        BrowserGlobal.iClickOn(loc.get(page,"input",field));
-        BrowserGlobal.iWaitForMilliseconds("500");
+        BrowserGlobal.iWaitForMilliseconds("2000");
         BrowserGlobal.iPressControlOrCommandAByOs();
-        BrowserGlobal.iWaitForMilliseconds("500");
+        BrowserGlobal.iInputInTo(text,loc.get(page,"input",field));
+    }
+
+    /**
+     * @param date [Date to fill]
+     * @param field [Field name]
+     * @param page [Page name]
+     */
+    @QAFTestStep(description = "MSDynamics: Input Date:{0} Field:{1} Page:{2}")
+    public static void inputDate(String date,String field,String page) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"input",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"input",field));
+        BrowserGlobal.iClickOn(loc.get(page,"input",field));
+        BrowserGlobal.iWaitForMilliseconds("2000");
+        BrowserGlobal.iClickOn(loc.get(page,"input",field));
+        BrowserGlobal.iWaitForMilliseconds("2000");
+        BrowserGlobal.iPressControlOrCommandAByOs();
+        BrowserGlobal.iWaitForMilliseconds("2000");
         BrowserGlobal.iInputInTo(date,loc.get(page,"input",field));
     }
 
-    @QAFTestStep(description = "MSDynamics: Input-lookup:{0} field:{1} page:{2}")
-    public static void inputLookUp(String value, String field, String page) throws Exception {
+    /**
+     * @param text [text to be entered in lookup]
+     * @param field [Field name]
+     * @param page [Page name]
+     */
+    @QAFTestStep(description = "MSDynamics: Input-Lookup Text:{0} Field:{1} Page:{2}")
+    public static void inputLookUp(String text, String field, String page) throws Exception {
         if (!field.contains(", Lookup")) {
             field = field +", Lookup";
         }
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"input",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"input",field));
         BrowserGlobal.iClickOn(loc.get(page,"input",field));
-        BrowserGlobal.iInputInTo(value, loc.get(page,"input",field));
+        BrowserGlobal.iInputInTo(text, loc.get(page,"input",field));
         BrowserGlobal.iWaitForMilliseconds("2000");
-        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"lookup-text",value));
-        BrowserGlobal.iClickOn(loc.get(page,"lookup-text",value));
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"lookup-text",text));
+        BrowserGlobal.iClickOn(loc.get(page,"lookup-text",text));
         BrowserGlobal.iWaitForMilliseconds("2000");
     }
 
-    @QAFTestStep(description="MSDynamics: Input-lookup:{0} field:{1} page:{2} wait-in-secs:{3}")
-    public void mSDynamicsInputLookupFieldPageWaitSecs(String value,String field,String page,String wait) throws Exception{
+    /**
+     * @param text [text to be entered in lookup]
+     * @param field [Field name]
+     * @param page [Page name]
+     * @param wait_in_secs [wait in Secs for Lookup results to populate]
+     */
+    @QAFTestStep(description="MSDynamics: Input-Lookup Text:{0} Field:{1} Page:{2} Wait-In-Secs:{3}")
+    public void inputLookUpWithWai(String text,String field,String page,String wait_in_secs) throws Exception{
         if (!field.contains(", Lookup")) {
             field = field +", Lookup";
         }
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"input",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"input",field));
         BrowserGlobal.iClickOn(loc.get(page,"input",field));
-        BrowserGlobal.iInputInTo(value, loc.get(page,"input",field));
-        BrowserGlobal.iWaitForSeconds(wait);
-        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"lookup-text",value));
-        BrowserGlobal.iClickOn(loc.get(page,"lookup-text",value));
-        BrowserGlobal.iWaitForSeconds(wait);
+        BrowserGlobal.iInputInTo(text, loc.get(page,"input",field));
+        BrowserGlobal.iWaitForSeconds(wait_in_secs);
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"lookup-text",text));
+        BrowserGlobal.iClickOn(loc.get(page,"lookup-text",text));
+        BrowserGlobal.iWaitForSeconds("2000");
     }
 
-    @QAFTestStep(description = "MSDynamics: Input-lookup:{0} field:{1} page:{2} by-clearing")
-    public static void inputLookUpByClearing(String value, String field, String page) throws Exception {
+    /**
+     * Clearing Existing value in Lookup ad then enter
+     * @param text [text to be entered in lookup]
+     * @param field [Field name]
+     * @param page [Page name]
+     */
+    @QAFTestStep(description = "MSDynamics: Input-Lookup Text:{0} By-Clearing-Existing-Value Field:{1} Page:{2}")
+    public static void inputLookUpByClearingExistingValue(String text, String field, String page) throws Exception {
         String splitFields[];
-        String delField = field;
+        String delField = field.replace(", Lookup","");
         if (field.contains(":")) {
             splitFields = field.split(":");
-            delField = splitFields[0];
+            delField = splitFields[0].replace(", Lookup","");
         }
-        BrowserGlobal.iClickOn("xpath=(//*[@title='"+delField.replace(", Lookup","")+"'])[2]");
+        if (!field.contains(", Lookup")) { field = field +", Lookup"; }
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"input",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"input",field));
+        BrowserGlobal.iClickOn("xpath=(//*[@title='"+delField+"'])[2]");
         BrowserGlobal.iWaitForMilliseconds("2000");
         BrowserGlobal.iClickOn(loc.get(page,"input","Delete"));
+        BrowserGlobal.iInputInTo(text, loc.get(page,"input",field));
+        BrowserGlobal.iWaitForMilliseconds("2000");
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"lookup-text",text));
+        BrowserGlobal.iClickOn(loc.get(page,"lookup-text",text));
+        BrowserGlobal.iWaitForMilliseconds("2000");
     }
 
-    @QAFTestStep(description = "MSDynamics: Input-lookup:{0} field:{1} page:{2} by-clearing-value:{3}")
-    public static void inputLookUpByClearingValue(String value, String field, String page, String clear_value) throws Exception {
+    /**
+     * Clearing Existing value by name in Lookup ad then enter
+     * Note: Should know existing value
+     * @param text [text to be entered in lookup]
+     * @param clear_value [Existing Value to clear in lookup]
+     * @param field [Field name]
+     * @param page [Page name]
+     */
+    @QAFTestStep(description = "MSDynamics: Input-Lookup Text:{0} By-Clearing-Value:{1} field:{2} page:{3}")
+    public static void inputLookUpByClearingValue(String text, String clear_value, String field, String page) throws Exception {
         String splitFields[];
-        String delField = field;
+        String delField = field.replace(", Lookup","");
         if (field.contains(":")) {
             splitFields = field.split(":");
-            delField = splitFields[0];
+            delField = splitFields[0].replace(", Lookup","");
         }
-        BrowserGlobal.iClickOn("xpath=(//*[@title='"+delField.replace(", Lookup","")+"'])[2]");
+        if (!field.contains(", Lookup")) { field = field +", Lookup"; }
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"input",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"input",field));
+        BrowserGlobal.iClickOn("xpath=(//*[@title='"+delField+"'])[2]");
         BrowserGlobal.iWaitForMilliseconds("2000");
         BrowserGlobal.iClickOn(loc.get(page,"input","Delete "+clear_value));
-//        BrowserGlobal.iInputInTo(value, loc.get(page,"input",field));
-//        BrowserGlobal.iWaitForMilliseconds("2000");
-//        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"lookup-text",value));
-//        BrowserGlobal.iClickOn(loc.get(page,"lookup-text",value));
-//        BrowserGlobal.iWaitForMilliseconds("2000");
-
+        BrowserGlobal.iInputInTo(text, loc.get(page,"input",field));
+        BrowserGlobal.iWaitForMilliseconds("2000");
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"lookup-text",text));
+        BrowserGlobal.iClickOn(loc.get(page,"lookup-text",text));
+        BrowserGlobal.iWaitForMilliseconds("1000");
     }
 
     /**
-     * @param menu [Menu name]
+     * @param menu_text [Menu Text to be clicked]
      */
-    @QAFTestStep(description = "MSDynamics: Click-Menu:{0}")
-    public static void clickMenu(String menu) throws Exception {
-        BrowserGlobal.iClickOn(loc.get("menu","link",menu));
+    @QAFTestStep(description = "MSDynamics: Click-Menu Text:{0}")
+    public static void clickMenuWithText(String menu_text) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get("menu","link",menu_text));
+        BrowserGlobal.iScrollToAnElement(loc.get("menu","link",menu_text));
+        BrowserGlobal.iClickOn(loc.get("menu","link",menu_text));
     }
 
     /**
-     * @param menu [Menu name]
+     * @param button_text [Button text to be clicked]
      */
-    @QAFTestStep(description = "MSDynamics: Click-Menu {0} and then Sub-Menu {1}")
-    public static void clickSubMenu(String menu,String subMenu) throws Exception {
-        BrowserGlobal.iClickOn(loc.get("menu","link",menu));
+    @QAFTestStep(description = "MSDynamics: Click-Menu-Button Text:{0}")
+    public static void clickMenuButtonWithText(String button_text) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get("menu","button",button_text));
+        BrowserGlobal.iScrollToAnElement(loc.get("menu","button",button_text));
+        BrowserGlobal.iClickOn(loc.get("menu","button",button_text));
+    }
+
+    /**
+     * @param tab_text [Tab text/name to be clicked]
+     */
+    @QAFTestStep(description = "MSDynamics: Click-Tab Text:{0}")
+    public static void clickTabWithText(String tab_text) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get("menu","tab",tab_text));
+        BrowserGlobal.iScrollToAnElement(loc.get("menu","tab",tab_text));
+        BrowserGlobal.iClickOn(loc.get("menu","tab",tab_text));
+    }
+
+    /**
+     * @param mainMenu_text [Main Menu Text/name to be clicked]
+     * @param subMenu_text [Sub Menu Text/name to be clicked]
+     */
+    @QAFTestStep(description = "MSDynamics: Click-Main-Menu Text:{0} Then-Sub-Menu Text:{1}")
+    public static void clickMainAndSubMenu(String mainMenu_text,String subMenu_text) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get("menu","link",mainMenu_text));
+        BrowserGlobal.iScrollToAnElement(loc.get("menu","link",mainMenu_text));
+        BrowserGlobal.iClickOn(loc.get("menu","link",mainMenu_text));
         BrowserGlobal.iWaitForSeconds("1");
-        BrowserGlobal.iClickOn(loc.get("menu","link",subMenu));
+        BrowserGlobal.iClickOn(loc.get("menu","link",subMenu_text));
+    }
+
+    /**
+     * @param main_button_text [Main Button Text/Name]
+     * @param sub_button_text [Sub Button Text/Name]
+     */
+    @QAFTestStep(description = "MSDynamics: Click-Main-Menu-Button Text:{0} Then-Sub-Menu-Button Text:{1}")
+    public static void clickMainAndSubMenuButton(String main_button_text,String sub_button_text) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get("menu","button",main_button_text));
+        BrowserGlobal.iScrollToAnElement(loc.get("menu","button",main_button_text));
+        BrowserGlobal.iClickOn(loc.get("menu","button",main_button_text));
+        BrowserGlobal.iWaitForSeconds("1");
+        BrowserGlobal.iClickOn(loc.get("menu","link",sub_button_text));
+    }
+
+    /**
+     * @param main_button_name [Main Menu name]
+     * @param wait_in_secs [Wait in Secs before clicking second Button in dropdown]
+     * @param sub_button_name [Sub Menu name]
+     */
+    @QAFTestStep(description = "MSDynamics: Click-Main-Menu-Button Text:{0} Wait-In Secs:{1} Sub-Menu-Button Text:{1} ")
+    public static void clickMainAndSubMenuButtonWithWaitInSecs(String main_button_name,String wait_in_secs, String sub_button_name) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get("menu","button",main_button_name));
+        BrowserGlobal.iScrollToAnElement(loc.get("menu","button",main_button_name));
+        BrowserGlobal.iClickOn(loc.get("menu","button",main_button_name));
+        BrowserGlobal.iWaitForSeconds(wait_in_secs);
+        BrowserGlobal.iClickOn(loc.get("menu","button",sub_button_name));
+    }
+
+    /**
+     * @param dropdown_Text [Text to be selected in dropdown]
+     * @param field [Field name]
+     * @param page [Page name]
+     */
+    @QAFTestStep(description = "MSDynamics: Select Text:{0} Field:{1} Page:{2}")
+    public static void selectByText(String dropdown_Text, String field, String page) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"select",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"select",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"select",field));
+        BrowserGlobal.iSelectDropdownWithText(loc.get(page,"select",field),dropdown_Text);
+    }
+
+    /**
+     * @param dropdown_Text_Index [Index of the Text to be selected in dropdown starting with 0]
+     * @param field [Field name]
+     * @param page [Page name]
+     */
+    @QAFTestStep(description = "MSDynamics: Select Index:{0} Field:{1} Page:{2}")
+    public static void selectByIndex(String dropdown_Text_Index, String field, String page) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"select",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"select",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"select",field));
+        BrowserGlobal.iSelectDropdownWithIndex(loc.get(page,"select",field),dropdown_Text_Index);
+    }
+
+    /**
+     * @param scroll_value [Scroll Value from the Visible Field]
+     * @param field [Field name]
+     * @param page [Page name]
+     */
+    @QAFTestStep(description = "MSDynamics: Scroll Value:{0} From-Visible Field:{1} Page:{2}")
+    public static void scrollFromVisibleField(String scroll_value, String field, String page) throws Exception {
+        BrowserGlobal.iWaitUntilElementPresent(loc.get(page,"general",field));
+        BrowserGlobal.iScrollToAnElement(loc.get(page,"general",field));
+        QAFWebDriver driver = new WebDriverTestBase().getDriver();
+        QAFWebElement point = new QAFExtendedWebElement(loc.get(page,"general",field));
+        WheelInput.ScrollOrigin scrollOrigin = WheelInput.ScrollOrigin.fromElement(point);
+        new Actions(driver).scrollFromOrigin(scrollOrigin,0 , Integer.parseInt(scroll_value)).perform();
     }
 }
-
-
-//     /*
-//         input(value,field,page)                                   D365: Input {0} into {1} in page {2}                    // Clear and Input
-//         inputDate(value,field,page)                               D365: Input date {0} into {1} in page {2}               // Double Click & Input
-//         inputDateByClearing(value,field,page)                     D365: Input date {0} by clearing {1} in page {2}    // Double Click, Ctrl-A & Input
-//         inputLookUp(value,field,page)                             D365: Input lookup {0} into {1} in page {2}
-//         inputLookUpWithWaitInSecs(value,wait-time,field,page)     D365: Input lookup {0} into {1} in page {2} with wait {3} secs
-//         clickMenu(menu)                                           D365: Click menu {0}
-//         clickSubMenu(main-menu,Sub-menu)                          D365: Click menu {0} and then sub menu {1}
-//         clickMenuButton(button)                                   D365: Click menu button {0}
-//         clickMenuButtonDropDown(button-1,button-2)                D365: Click menu button dropdown {0} and then {1}
-//         clickLink(link,page)                                      D365: Click link {0} in page {2}
-//         clickTab(tab,page)                                        D365: Click tab {0} in page {2}
-//         selectByText(value,field,page)                            D365: Select text {0} in {} in page {2}
-//         selectByIndex(value,field,page)                           D365: Select text index {0} in {} in page {2}
-//         selectByValue(value,field,page)                           D365: Select text Value {0} in {} in page {2}
-//         scrollFromVisibleField(scroll-value,field,page)           D365: Scroll {0} from visible field {} in page {2}
-
-//         D365Global.input("Renish","First Name","Recruiter") // 6.5 6.6
-
-//      */
-// //    @QAFTestStep(description = "D365: Input lookup {0} into {1} with heading {2}")
-// //    public static void inputLookUpWithHeading(String value,String locator, String heading) throws Exception {
-// //        BrowserGlobal.iClickOn(locator);
-// //        BrowserGlobal.iClearAndFillInTo(value, locator);
-// //        BrowserGlobal.iWaitForMilliseconds("2000");
-// //        BrowserGlobal.iPressKey("ARROW_DOWN");
-// //        BrowserGlobal.iWaitForMilliseconds("2000");
-// //        BrowserGlobal.iPressReturnOrEnterKey();
-// //    }
