72c327a8b542edc43665b0fd3831802b33820ed6
coderrenish <coderrenish@users.noreply.github.com>
2024-01-16 13:02:50 +1000

    Changes

--- src/test/java/com/ahq/utils/loc.java
+++ src/test/java/com/ahq/utils/loc.java
@@ -58,10 +58,26 @@ public class loc {
                     getBundle().setProperty(locator,"{\"locator\":[\"xpath=(//input[@placeholder='Filter by keyword'])["+fieldName.trim()+"]\",\"xpath=(//*[contains(@data-id,'quickFind_text')])["+fieldName.trim()+"]\"],\"desc\":\""+fieldName+" field\"}");
                 } else if (fieldType.trim().equalsIgnoreCase("d365_button")){
                     getBundle().setProperty(locator,"{\"locator\":[\"xpath=//button[@aria-label='"+fieldName.trim()+"']\",\"xpath=//button[@title='"+fieldName.trim()+"']\",\"xpath=//button[contains(@title,'"+fieldName.trim()+"')]\"],\"desc\":\""+fieldName+" field\"}");
+                } else if (fieldType.trim().equalsIgnoreCase("d365_button_popup")){
+                    getBundle().setProperty(locator,"{\"locator\":[\"xpath=//div[contains(@id,'dialogView')]/descendant::button[@aria-label='"+fieldName.trim()+"']\",\"xpath=//div[contains(@id,'dialogView')]/descendant::span[text()='"+fieldName.trim()+"']\"],\"desc\":\""+fieldName+" : Popup button field\"}");
                 } else if (fieldType.trim().equalsIgnoreCase("d365_select")){
                     getBundle().setProperty(locator,"{\"locator\":[\"xpath=//select[@aria-label='"+fieldName.trim()+"']\"],\"desc\":\""+fieldName+" select field\"}");
-                }else{
-                    getBundle().setProperty(locator,"{\"locator\":[\"xpath=//*[@aria-label='"+fieldName.trim()+"']\",\"xpath=//*[@title='"+fieldName.trim()+"']\",\"xpath=//*[@placeholder='"+fieldName.trim()+"']\",\"xpath=//*[@value='"+fieldName.trim()+"']\",\"id="+fieldName.replaceAll("\\s+","").toLowerCase()+"\",\"name="+fieldName.replaceAll("\\s+","").toLowerCase()+"\",\"xpath=//*[contains(@aria-label,'"+fieldName.trim()+"')]\",\"xpath=//*[contains(@title,'"+fieldName.trim()+"')]\",\"xpath=//*[contains(@value,'"+fieldName.trim()+"')]\",\"xpath=//*[contains(text(),'"+fieldName.trim()+"')]\"],\"desc\":\""+fieldName+" field\"}");
+                } else if (fieldType.trim().equalsIgnoreCase("d365_header_control_list")){
+                    getBundle().setProperty(locator,"{\"locator\":[\"xpath=//div[contains(@id ,'headerControlsList')]//descendant::div[text()='"+fieldName.trim()+"']\"],\"desc\":\""+fieldName+": Header Control List Text\"}");
+                } else if (fieldType.trim().equalsIgnoreCase("d365_error_text")){
+                    getBundle().setProperty(locator,"{\"locator\":[\"xpath=//span[contains(@id,'error-message')][contains(text(),'"+fieldName.trim()+"']\"],\"desc\":\""+fieldName+": Error Message Text\"}");
+                } else if (fieldType.trim().equalsIgnoreCase("d365_show_hide_button")){
+                    getBundle().setProperty(locator,"{\"locator\":[\"xpath=//i[@title='Show/Hide "+fieldName.trim()+"']\",\"xpath=//i[contains(@title='Show/Hide "+fieldName.trim()+"')]\"],\"desc\":\""+fieldName+": Show Hide Button\"}");
+                } else if (fieldType.trim().equalsIgnoreCase("d365_table_header")){
+                    String[] headerElementSplit = fieldName.trim().split(":");
+                    getBundle().setProperty(locator,"{\"locator\":[\"xpath=((//div[@class='ag-header-container'])[1]/descendant::div[@aria-colindex='"+headerElementSplit[1]+"']/descendant::div[text()='"+headerElementSplit[0]+"'])[1]\"],\"desc\":\""+headerElementSplit[0]+" Table Header\"}");
+                } else if (fieldType.trim().equalsIgnoreCase("d365_table_header_edit_column")){
+                    String[] headerElementSplit = fieldName.trim().split(":");
+                    getBundle().setProperty(locator,"{\"locator\":[\"xpath=(//div[@role='listitem'])["+headerElementSplit[1]+"][@aria-label='"+headerElementSplit[0]+"']/descendant::span[text()='"+headerElementSplit[0]+"']\"],\"desc\":\""+headerElementSplit[0]+" Table Header\"}");
+                } else if (fieldType.trim().equalsIgnoreCase("d365_table_horizontal_scroll")){
+                    getBundle().setProperty(locator,"{\"locator\":[\"xpath=(//div[@class='ag-body-horizontal-scroll-viewport'])[1]\"],\"desc\":\""+fieldName+": Show Hide Button\"}");
+                } else{
+                    getBundle().setProperty(locator,"{\"locator\":[\"xpath=//*[@aria-label='"+fieldName.trim()+"']\",\"xpath=//*[@title='"+fieldName.trim()+"']\",\"xpath=//*[@placeholder='"+fieldName.trim()+"']\",\"xpath=//*[@value='"+fieldName.trim()+"']\",\"id="+fieldName+"\",\"id="+fieldName.replaceAll("\\s+","").toLowerCase()+"\",\"name="+fieldName.replaceAll("\\s+","").toLowerCase()+"\",\"xpath=//*[contains(@aria-label,'"+fieldName.trim()+"')]\",\"xpath=//*[contains(@title,'"+fieldName.trim()+"')]\",\"xpath=//*[contains(@value,'"+fieldName.trim()+"')]\",\"xpath=//*[contains(text(),'"+fieldName.trim()+"')]\"],\"desc\":\""+fieldName+" field\"}");
                 }
                 System.out.println("==== AUTO GENERATED LOCATOR [3]====> " + locator + "=" + getBundle().getPropertyValue(locator));
             }
